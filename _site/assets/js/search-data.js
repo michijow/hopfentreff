19c612706d0b2496e041b053cb022d144543d7ca
zott <>
2025-02-22 14:42:56 +0100

    updated site name

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -1,8 +1,8 @@
 var store = [{
-        "title": "Schinken-P.",
+        "title": "Schinken-Peter",
         "excerpt":"Wirtshaus in Giesing. Bewertung                 Hopfen                         &#x1F37A;                    &#x1F37A;                    &#x1F37A;                    &#x1F37A;                    &#x1F37A;                Käsepätzle                         &#x1F9C0;                    &#x1F9C0;                    &#x1F9C0;                    &#x1F9C0;                    &#x1F9C0;                Treff                         &#128588;                    &#128588;                    &#128588;                    &#128588;                    &#128588;            ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/hopfentreffen/schinken-p/"
+        "url": "http://localhost:4000/hopfentreffen/schinken-peter/"
       },{
         "title": "Schinken-P.",
         "excerpt":"Wirtshaus in Giesing. Käsespätzle 6/10Bier 6/10 ","categories": [],
