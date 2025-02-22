71b1ca316c82ac9b2117e04f39febd535205a3f2
zott <>
2025-02-22 14:32:52 +0100

    added rating

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Schinken-P.",
-        "excerpt":"Wirtshaus in Giesing. Bewertung Käsespätzle 6/10.Bier 6/10. ","categories": [],
+        "excerpt":"Wirtshaus in Giesing. Bewertung                 Hopfen                         &#x1F37A;                    &#x1F37A;                    &#x1F37A;                    &#x1F37A;                    &#x1F37A;                Käsepätzle                         &#x1F9C0;                    &#x1F9C0;                    &#x1F9C0;                    &#x1F9C0;                    &#x1F9C0;                Treff                         &#128588;                    &#128588;                    &#128588;                    &#128588;                    &#128588;            ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/hopfentreffen/schinken-p/"
       },{
