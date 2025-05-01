cc21f462a696ce106aa28ce6f038264afef670a3
michijow <93324565+michijow@users.noreply.github.com>
2025-05-01 10:57:58 +0200

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function getFirstThursdayOfMonth(year, month) {
 }
 
 // Optional: Set a custom target date here (year, monthIndex (0–11), day, hour, minute)
-let customDate = new Date(2025, 3, 11, 18, 45, 0); // May 10, 2025 at 8:00 PM
+let customDate = new Date(2025, 5, 1, 12, 30, 0); // May 10, 2025 at 8:00 PM
 
 function updateCountdown() {
     const now = new Date();
